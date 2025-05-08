2c434add62368064e4509de5637fb8c3e7481a47
zylhy <92566572+zylhy@users.noreply.github.com>
2025-05-08 17:58:26 +0800

    优化初始化界面

--- 桌面哔哩哔哩/js/init.js
+++ 桌面哔哩哔哩/js/init.js
@@ -1,43 +1,103 @@
-//初始化 计算图片位置
-
 class Wallpaper {
-    constructor() {
-        if (Wallpaper.instance) return Wallpaper.instance
-        this.img = null;    //图片对象
-        this.bgLeft = document.querySelector('.bgLeft')
-        this.bgRight = document.querySelector('.bgRight')
-        this.bgWrap = document.querySelector('.bgWrap')
-        this.init()
+    #img = null;
+    #imgSrc = 'assets/bg.jpg';
+    #bgLeft = null;
+    #bgRight = null;
+    #bgWrap = null;
+    #triggerArea = null;
+    #triggerAreaSize = 50;
+    #resizeTimer = null;
+    #contentWidth = 200;
+    #content = null;
+    static instance = null;
+
+    constructor(option) {
+        if (Wallpaper.instance) return Wallpaper.instance;
+        this.#contentWidth = option?.contentWidth ?? this.#contentWidth;
+        this.#imgSrc = option?.imgSrc ?? this.#imgSrc;
+        this.#triggerAreaSize = option?.triggerAreaSize ?? this.#triggerAreaSize;
+        this.#bgLeft = document.querySelector('.bgLeft');
+        this.#bgRight = document.querySelector('.bgRight');
+        this.#bgWrap = document.querySelector('.bgWrap');
+        this.#triggerArea = document.querySelector('.triggerArea');
+        this.#content = document.querySelector('.content');
+        if (!this.#bgLeft || !this.#bgRight || !this.#bgWrap || !this.#triggerArea||!this.#content) {
+            throw new Error('Wallpaper 初始化失败，DOM 元素缺失');
+        }
+        this.init();
         Wallpaper.instance = this;
     }
+    #onResize = () => {
+        clearTimeout(this.#resizeTimer);
+        this.#resizeTimer = setTimeout(() => {
+            this.#setImage();
+        }, 100);
+    }
 
-    init() {
-        this.img = new Image()
-        this.img.src = './assets/bg.jpg'
-        this.img.onload = () => {
-            // 如果图片比例>=屏幕，不用处理
-            if (this.bgWrap.clientHeight / this.bgWrap.clientWidth >= this.img.height / this.img.width) {
-                // 两个图片水平的和是屏幕的一半即可对齐
-                let offset = (this.img.height * this.bgWrap.clientHeight / this.bgWrap.clientWidth - this.bgWrap.clientWidth) / 2
-                console.log(this.img.height * this.bgWrap.clientHeight / this.bgWrap.clientWidth ,this.bgWrap.clientWidth)
-                offset=0
-                this.bgLeft.style.backgroundPosition = `${offset}px center `
-                this.bgRight.style.backgroundPosition = `${offset - this.bgLeft.clientWidth}px center `
-                this.bgLeft.style.backgroundSize = this.bgRight.style.backgroundSize = "auto 100%"
+    async init() {
 
-            } else {
-                // 如果图片比例<屏幕，则图片宽度应该是两倍屏幕宽
-                this.bgLeft.style.backgroundSize = this.bgRight.style.backgroundSize = "200% auto"
-                this.bgLeft.style.backgroundPosition = `left center `
-                this.bgRight.style.backgroundPosition = `right center `
+        if (!this.#img) {
+            try {
+                await this.#loadImg();
+            } catch (err) {
+                console.warn('图片加载失败:', err);
+                return;
             }
-
         }
-        this.img.onerror = function (err) {
-            console.log(err, '图片加载出错')
-        }
+        this.#setImage();
+        window.addEventListener('resize', this.#onResize);
+        this.#content.style.width = `${this.#contentWidth}px`;
+        this.#expandContent();
+    }
 
+    #loadImg() {
+        return new Promise((resolve, reject) => {
+            this.#img = new Image();
+            this.#img.src = this.#imgSrc;
+            this.#img.onload = () => resolve();
+            this.#img.onerror = reject;
+        });
+    }
+
+    #setImage() {
+        const wrapRatio = this.#bgWrap.clientHeight / this.#bgWrap.clientWidth;
+        const imgRatio = this.#img.height / this.#img.width;
+
+        if (wrapRatio >= imgRatio) {
+            const scaledWidth = this.#bgWrap.clientHeight * this.#img.width / this.#img.height;
+            const offset = -(scaledWidth - this.#bgWrap.clientWidth) / 2;
+            this.#bgLeft.style.backgroundPosition = `${offset}px center`;
+            this.#bgRight.style.backgroundPosition = `${offset - this.#bgLeft.clientWidth}px center`;
+            this.#bgLeft.style.backgroundSize = this.#bgRight.style.backgroundSize = "auto 100%";
+        } else {
+            this.#bgLeft.style.backgroundSize = this.#bgRight.style.backgroundSize = "200% auto";
+            this.#bgLeft.style.backgroundPosition = "left center";
+            this.#bgRight.style.backgroundPosition = "right center";
+        }
+        this.#bgLeft.style.backgroundImage = `url('${this.#imgSrc}')`;
+        this.#bgRight.style.backgroundImage = `url('${this.#imgSrc}')`;
+    }
+    #expandContent() {
+        this.#triggerArea.addEventListener('mouseenter', (e) => {
+            this.#bgLeft.style.transform = `translateX(-${this.#contentWidth / 2}px)`;
+            this.#bgRight.style.transform = `translateX(${this.#contentWidth / 2}px)`;
+            this.#triggerArea.style.width = `${this.#contentWidth}px`;
+            this.#triggerArea.style.pointerEvents = `none`;
+
+        })
+        // this.#triggerArea.addEventListener('mouseleave', (e) => {
+        //     this.#bgLeft.style.transform = `translateX(0)`;
+        //     this.#bgRight.style.transform = `translateX(0)`;
+        //     this.#triggerArea.style.width = `${this.#triggerAreaSize}px`;
+        // })
+    }
+    destroy() {
+        window.removeEventListener('resize', this.#onResize);
+        Wallpaper.instance = null;
     }
 }
 let wallpaper = new Wallpaper()
-window.addEventListener('resize', () => wallpaper.init())
+
+
+
+
